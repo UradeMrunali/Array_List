47de23c2d571a8f682def5226cfcb40d34c89c83
Mrunali <mrunaliurade26@gmail.com>
2023-06-21 11:04:48 +0530

    example of array list

---
+++ Assign2.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Assign2 {
+
+	public static void main(String[] args) {
+		List<String> s=new ArrayList<String>();
+		
+		s.add("Red");
+		s.add("Green");
+		s.add("Orange");
+		s.add("White");
+		s.add("Black");
+		
+		 for(String e:s){
+		System.out.println(e);
+	}
+}
+}
